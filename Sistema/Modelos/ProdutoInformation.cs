74e27288dcd3f252b7eaf7943311ddee6a1637ff
2024.4.14 <2024.4.14@DESKTOP-I4U5BN5>
2025-04-30 19:03:20 -0300

    configuracaoprodutoinfortacion

--- Sistema/Modelos/ProdutoInformation.cs
+++ Sistema/Modelos/ProdutoInformation.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace Sistema.Modelos
 {
-    internal class ProdutoInformation
+    public class ProdutoInformation
     {
+        private int IdProduto { get; set; }
+        private DateTime DataCadastro { get; set; }
+        private string Produto { get; set; }
+        private int NumeroSerie { get; set; }
+        private decimal ValorCompra { get; set;
+        private decimal ValorVenda { get; set; }
+        private int Quantidade { get; set; }
+        private decimal MargemLucro { get; set; }
+        private string Observacao { get; set; }
+
     }
 }
